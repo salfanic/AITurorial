9f9b90befab8250c109ebe04da18dc455da606cc
正田 <salfanic@gmail.com>
2023-03-10 02:07:16 +0900

    アビリティ追加

---
+++ Source/MyProject/MyGameplayAbility.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameplayAbility.h"
+#include "AbilitySystemComponent.h"
+#include "GameplayTagContainer.h"
+
+UMyGameplayAbility::UMyGameplayAbility()
+{
+	// Default to Instance Per Actor
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+
+	// Default tags that block this ability from activating
+	ActivationBlockedTags.AddTag(FGameplayTag::RequestGameplayTag(FName("State.Dead")));
+	ActivationBlockedTags.AddTag(FGameplayTag::RequestGameplayTag(FName("State.Debuff.Stun")));
+}
+
+void UMyGameplayAbility::OnAvatarSet(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec)
+{
+	Super::OnAvatarSet(ActorInfo, Spec);
+
+	if (ActivateAbilityOnGranted)
+	{
+		ActorInfo->AbilitySystemComponent->TryActivateAbility(Spec.Handle, false);
+	}
+}
