6c79506980debf6e8f834ffd4d2933f15bc1cc87
salfa <salfanic@gmail.com>
2023-03-10 18:02:09 +0900

    アビリティ追加中

--- Source/MyProject/MyGameplayAbility.cpp
+++ Source/MyProject/MyGameplayAbility.cpp
@@ -9,10 +9,6 @@ UMyGameplayAbility::UMyGameplayAbility()
 {
 	// Default to Instance Per Actor
 	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
-
-	// Default tags that block this ability from activating
-	ActivationBlockedTags.AddTag(FGameplayTag::RequestGameplayTag(FName("State.Dead")));
-	ActivationBlockedTags.AddTag(FGameplayTag::RequestGameplayTag(FName("State.Debuff.Stun")));
 }
 
 void UMyGameplayAbility::OnAvatarSet(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec)
